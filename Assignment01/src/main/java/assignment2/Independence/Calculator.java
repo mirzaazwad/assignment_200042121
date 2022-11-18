82d329a5aa727e65b823e65deecb6148e35e08f7
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2022-11-18 17:00:50 +0600

    Add files via upload

---
+++ Assignment01/src/main/java/assignment2/Independence/Calculator.java
@@ -0,0 +1,9 @@
+package assignment2.Independence;
+
+public class Calculator {
+    public int Sum(int a,int b){
+        Numbers numbers=new Numbers(2,3);
+        numbers.Sum();
+        return numbers.getResult();
+    }
+}
